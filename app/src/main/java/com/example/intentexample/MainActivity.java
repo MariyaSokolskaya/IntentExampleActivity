0f7d3a8e68b07e8b3925618680776aa93a052ae5
MariyaSokolskaya <faenna@inbox.ru>
2025-11-28 22:59:42 +0800

    Контекстное меню

--- app/src/main/java/com/example/intentexample/MainActivity.java
+++ app/src/main/java/com/example/intentexample/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.intentexample;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -15,5 +19,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.to_about){
+            Intent intent = new Intent(this, AboutActivity.class);
+            startActivity(intent);
+        }
+        if (item.getItemId() == R.id.exit){
+            finish();
+        }
+        return true;
     }
 }
